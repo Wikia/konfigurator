7355323e6f2c176e98e430b181fea7c9bec56d68
Łukasz Harasimowicz <dev@harnash.eu>
2017-07-21 16:16:27 +0200

    More test cases for variable definition parsing

--- config/config_test.go
+++ config/config_test.go
@@ -15,6 +15,8 @@ var _ = Describe("Config", func() {
 			"bar":       "consul(key2)",
 			"simple":    "simple(key3)",
 			"reference": "simple(node.name)->reference",
+			"simple2":   "simple(key4)->config",
+			"simple3":   "simple(key5)->secret",
 		}
 
 		expectedDefinitions := []interface{}{
@@ -22,6 +24,8 @@ var _ = Describe("Config", func() {
 			model.VariableDef{Name: "bar", Value: "key2", Type: model.CONFIGMAP, Source: model.CONSUL},
 			model.VariableDef{Name: "simple", Value: "key3", Type: model.CONFIGMAP, Source: model.SIMPLE},
 			model.VariableDef{Name: "reference", Value: "node.name", Type: model.REFERENCE, Source: model.SIMPLE},
+			model.VariableDef{Name: "simple2", Value: "key4", Type: model.CONFIGMAP, Source: model.SIMPLE},
+			model.VariableDef{Name: "simple3", Value: "key5", Type: model.SECRET, Source: model.SIMPLE},
 		}
 
 		It("should properly parse variable definitions", func() {

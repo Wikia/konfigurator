75791e7f52490db787a1757b968cab8276b4b2be
Łukasz Harasimowicz <dev@harnash.eu>
2017-07-25 16:29:44 +0200

    Added more tests

--- config/config_test.go
+++ config/config_test.go
@@ -3,13 +3,17 @@ package config_test
 import (
 	. "github.com/Wikia/konfigurator/config"
 
+	"strings"
+
 	"github.com/Wikia/konfigurator/model"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var _ = Describe("Config", func() {
-	Context("with sample configuration", func() {
+	Context("with sample variable configuration", func() {
 		conf := map[string]string{
 			"foo":       "vault(key1)",
 			"bar":       "consul(key2)",
@@ -35,6 +39,53 @@ var _ = Describe("Config", func() {
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(ret).Should(ConsistOf(expectedDefinitions...))
+		})
+	})
+
+	Context("with sample configuration", func() {
+		conf := `
+LogLevel: debug
+Consul:
+  Address: consul:8500
+  Datacenter: dev
+  TlsSkipVerify: true
+  Token: 123foo
+Vault:
+  Address: https://vault:8200
+  TlsSkipVerify: true
+Application:
+  name: app
+  namespace: dev
+  definitions:
+    foo: simple(bar)`
+
+		var testCmd = &cobra.Command{}
+
+		It("should read config without error", func() {
+			viper.SetConfigType("yaml")
+			err := viper.ReadConfig(strings.NewReader(conf))
+			Expect(err).NotTo(HaveOccurred())
+
+			err = Setup(testCmd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(viper.GetString("vault.tokenpath")).NotTo(BeEmpty())
+
+			config := Config{}
+			err = viper.Unmarshal(&config)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(config).NotTo(BeNil())
+			Expect(config.LogLevel).To(Equal("debug"))
+			Expect(config.Consul.Address).To(Equal("consul:8500"))
+			Expect(config.Consul.Datacenter).To(Equal("dev"))
+			Expect(config.Consul.TLSSkipVerify).To(Equal(true))
+			Expect(config.Consul.Token).To(Equal("123foo"))
+			Expect(config.Vault.Address).To(Equal("https://vault:8200"))
+			Expect(config.Vault.TLSSkipVerify).To(Equal(true))
+			Expect(config.Application.Name).To(Equal("app"))
+			Expect(config.Application.Namespace).To(Equal("dev"))
+			Expect(config.Application.Definitions).NotTo(BeEmpty())
+			Expect(config.Application.Definitions["foo"]).To(Equal("simple(bar)"))
 		})
 	})
 })

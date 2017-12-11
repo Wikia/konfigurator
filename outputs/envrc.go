897761c2f741ceca058f372b7eceaba85dcc08fd
Łukasz Harasimowicz <dev@harnash.eu>
2017-12-11 13:22:55 +0100

    Fix envrc escaping

--- outputs/envrc.go
+++ outputs/envrc.go
@@ -5,8 +5,6 @@ import (
 
 	"strings"
 
-	"regexp"
-
 	"io"
 
 	"github.com/Wikia/konfigurator/model"
@@ -14,17 +12,18 @@ import (
 
 type OutputEnvrc struct{}
 
-var escapeRegex = regexp.MustCompile(`([$\\_\x96])`)
-
 func (o *OutputEnvrc) Save(name string, namespace string, writer io.Writer, vars []model.Variable) error {
 	for _, variable := range vars {
 		if variable.Type == model.REFERENCED {
 			continue
 		}
 
-		value := escapeRegex.ReplaceAllString(variable.Value.(string), "\\$1")
+		value := fmt.Sprintf("%s", variable.Value)
+		if strings.Contains(value, "(") {
+			value = fmt.Sprintf("\"%s\"", value)
+		}
 
-		_, err := fmt.Fprintf(writer, "export %s=\"%s\"\n", strings.ToUpper(variable.Name), value)
+		_, err := fmt.Fprintf(writer, "export %s=%s\n", strings.ToUpper(variable.Name), value)
 
 		if err != nil {
 			return err
